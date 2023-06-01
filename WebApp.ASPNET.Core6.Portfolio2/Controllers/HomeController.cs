906059636a8256b9e546aca594e4b1de4684fb64
Rafael Vieira Suarez <rafael.suarez@compasso.com.br>
2023-06-01 00:32:24 -0300

    Adicionar arquivos de projeto.

---
+++ WebApp.ASPNET.Core6.Portfolio2/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.ASPNET.Core6.Portfolio2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
